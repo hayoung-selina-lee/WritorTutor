6b9986827cdc52bca66ea3954814e99336415fcf
leehy0321 <sgn0321@naver.com>
2026-05-10 01:48:04 -0700

    add logo image file with title and create Navbar page with logo, navigation links, and mypage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,28 @@
+import { Link } from "react-router-dom";
+import LogoImage from "../assets/logo_title.svg";
+
 function Navbar() {
-  return <nav className="p-4 border-b">Writor NavBar</nav>;
+  return (
+    <nav className="p-4 border-b w-full">
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row gap-3 items-center">
+          <Link to="/">
+            <img src={LogoImage} className="w-60" alt="WriteTutor logo" />
+          </Link>
+          <div className="pl-3 flex font-semibold flex-row gap-5">
+            <Link className="hover:text-primary" to="/">
+              HOME
+            </Link>
+            <Link className="hover:text-primary" to="/history">
+              HISTORY
+            </Link>
+          </div>
+        </div>
+        <Link className="font-semibold hover:text-primary" to="/mypage">
+          MY PAGE
+        </Link>
+      </div>
+    </nav>
+  );
 }
 export default Navbar;
